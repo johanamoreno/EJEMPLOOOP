eb17753d41613cc70b71f7c8f9e251717683f569
johanamoreno <71187100+johanamoreno@users.noreply.github.com>
2020-09-22 12:19:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hotel WCG!");
+
+            Console.WriteLine("Cantidad Huespedes");
+            int huespedes = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dias de estadia");
+            int cant_dias = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nombre de Habitacion");
+            string nom_hab = Console.ReadLine();
+
+            //Instancia
+
+            var hotelUno = new GestionHotel()
+            {
+                habitacion = nom_hab,
+                huesped = huespedes,
+                dias = cant_dias
+            };
+
+            hotelUno.cuentaCobro();
+        }
+    }
+}
